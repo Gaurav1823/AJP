a58678007f535e60998aa8bb8cd280f5c32b0d01
Gaurav1823 <140207018+Gaurav1823@users.noreply.github.com>
2025-07-07 13:22:03 +0530

    Create CreateEmployeeTable.java

---
+++ JDBC/CreateEmployeeTable.java
@@ -0,0 +1,44 @@
+package multithreading;
+
+import java.sql.*;
+
+
+
+public class CreateEmployeeTable {
+    public static void main(String[] args) {
+    	String url = "jdbc:mysql://localhost:3306/myjavadb";
+        String user = "root";
+        String password = "@Sulipsa4ever";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement()) {
+
+           
+            String createTable = """
+                CREATE TABLE IF NOT EXISTS Employee (
+                    eid INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL,
+                    address VARCHAR(255) NOT NULL,
+                    salary INT NOT NULL
+                )
+                """;
+            stmt.executeUpdate(createTable);
+            System.out.println("Employee table created .");
+
+       
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Employee");
+
+            System.out.println("Employee Records:");
+            while (rs.next()) {
+                System.out.printf("ID: %d | Name: %s | Address: %s | Salary: ₹%d%n",
+                        rs.getInt("eid"),
+                        rs.getString("name"),
+                        rs.getString("address"),
+                        rs.getInt("salary"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
