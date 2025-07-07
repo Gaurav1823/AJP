4981a40522497f6429ba68f799b8269d441f005f
Gaurav1823 <140207018+Gaurav1823@users.noreply.github.com>
2025-07-07 13:23:18 +0530

    Create MySQLConnectionTest.java

---
+++ JDBC/MySQLConnectionTest.java
@@ -0,0 +1,20 @@
+package multithreading;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class MySQLConnectionTest {
+	public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/myjavadb";
+        String user = "root"; // your MySQL username
+        String password = "@Sulipsa4ever"; // your MySQL password
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection: " + connection); // Print reference to confirm success
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+	}
+}
